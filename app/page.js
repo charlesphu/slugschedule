92f950008f7f53a8be04e404300171daf942cda2
Dino <73320617+GreenDeno@users.noreply.github.com>
2025-04-11 22:02:54 -0700

    Add simple pdf file upload

--- app/page.js
+++ app/page.js
@@ -1,15 +1,40 @@
 "use client";
 
-import React from "react";
-import { sendPrompt } from "./hooks/aiPrompt";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [fileName, setFileName] = React.useState("No transcript selected");
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setFileName(file.name);
+    }
+  };
+
+  const fileInputRef = React.useRef(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
   return (
-    <button
-      onClick={() => {
-        sendPrompt("Hello");
-      }}>
-      Hello World
-    </button>
+    <div className="flex h-screen flex-col items-center justify-center gap-4">
+      <input
+        type="file"
+        accept=".pdf"
+        ref={fileInputRef}
+        onChange={handleFileChange}
+        className="hidden"
+      />
+
+      <button
+        onClick={handleButtonClick}
+        className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+        Choose PDF File
+      </button>
+
+      <p>{fileName}</p>
+    </div>
   );
 }
