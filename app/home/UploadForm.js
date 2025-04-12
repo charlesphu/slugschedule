6178264a1721777c53e7ac5d7a2fa6385c9d43fd
Dino <73320617+GreenDeno@users.noreply.github.com>
2025-04-12 16:19:17 -0700

    Add back UploadForm and implement animate spinner while transcript process

--- app/home/UploadForm.js
+++ app/home/UploadForm.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { usePDFToText } from "../hooks/usePDFToText";
 import { UserDataContext } from "../layout";
@@ -8,9 +8,11 @@ import { useRouter } from "next/navigation";
 import { useWebscrape } from "../hooks/webscrape"; // Import the web scraping hook
 
 function UploadArea() {
-  const { setTranscriptData } = useContext(UserDataContext);
   const [isFileUploading, setIsFileUploading] = useState(false);
   const [isFileUploaded, setIsFileUploaded] = useState(false);
+  const [isFileProcessing, setIsFileProcessing] = useState(false);
+
+  const { setTranscriptData } = useContext(UserDataContext);
   const router = useRouter();
 
   const onDrop = async (acceptedFiles) => {
@@ -19,6 +21,9 @@ function UploadArea() {
     const file = acceptedFiles[0];
     const pdfURL = URL.createObjectURL(file);
     const pdfText = await usePDFToText(pdfURL);
+
+    setIsFileUploaded(true);
+    setIsFileProcessing(true);
 
     try {
       console.log("Extracted PDF Text:", pdfText);
@@ -53,7 +58,7 @@ function UploadArea() {
     } catch (error) {
       console.error("Error processing the file:", error);
     } finally {
-      setIsFileUploaded(true);
+      // setIsFileProcessing(false); // We want the spinner to stay until the routing is done (for now)
       setIsFileUploading(false);
     }
     await handleScrapeAndSendToGemini();
@@ -61,7 +66,7 @@ function UploadArea() {
 
   const handleScrapeAndSendToGemini = async () => {
     const url =
-     "https://catalog.ucsc.edu/en/current/general-catalog/academic-units/baskin-engineering/computer-science-and-engineering/computer-science-bs/";
+      "https://catalog.ucsc.edu/en/current/general-catalog/academic-units/baskin-engineering/computer-science-and-engineering/computer-science-bs/";
 
     try {
       // Scrape both URLs
@@ -97,6 +102,19 @@ function UploadArea() {
     disabled: isFileUploading || isFileUploaded,
   });
 
+  const [isDragHovered, setIsDragHovered] = useState(isDragActive);
+  useEffect(() => {
+    if (isDragActive) {
+      setIsDragHovered(true);
+    } else {
+      // Add a small delay before setting isDragHovered to false
+      // This prevents flickering when user is moving the file around
+      setTimeout(() => {
+        setIsDragHovered(false);
+      }, 100); // 150ms delay
+    }
+  }, [isDragActive]);
+
   return (
     <div
       className="flex h-60 cursor-pointer flex-col items-center justify-center rounded-md border-4 border-dashed border-[var(--text-primary)] bg-[var(--container-secondary)] p-2"
@@ -105,22 +123,31 @@ function UploadArea() {
         if (!isFileUploading || isFileUploaded) {
           open();
         }
-      }}
-    >
+      }}>
       <input
         {...getInputProps()}
         disabled={isFileUploading || isFileUploaded}
       />
       {isFileUploaded ? (
-        <p className="text-center text-3xl font-bold text-[var(--text-primary)] select-none">
-          Transcript Received
-        </p>
-      ) : (
         <>
           <p className="text-center text-3xl font-bold text-[var(--text-primary)] select-none">
-            {isDragActive ? "Drop file here..." : "Upload your Transcript"}
+            Transcript Received
           </p>
-          {!isDragActive && (
+          {isFileProcessing && (
+            <div className="mt-5 h-10 w-10 animate-spin rounded-full border-4 border-[var(--text-primary)] border-t-transparent"></div>
+          )}
+        </>
+      ) : (
+        <>
+          <p
+            className="text-center text-3xl font-bold text-[var(--text-primary)] select-none"
+            style={{
+              scale: isDragHovered ? 1.3 : 1,
+              transition: "scale 0.1s ease-in-out",
+            }}>
+            {isDragHovered ? "Drop file here..." : "Upload your Transcript"}
+          </p>
+          {!isDragHovered && (
             <p className="text-center text-xl text-[var(--text-primary)] select-none">
               Drag and Drop or <span className="underline">Click Here</span>
             </p>
@@ -131,4 +158,33 @@ function UploadArea() {
   );
 }
 
-export default UploadArea;
+export default function UploadForm() {
+  const [isHoveringUpload, setIsHoveringUpload] = useState(false);
+
+  return (
+    <section
+      className="flex w-[45%] flex-col gap-5 rounded-xl bg-[var(--container-primary)] p-6 shadow-md"
+      onMouseEnter={() => setIsHoveringUpload(true)}
+      onMouseLeave={() => setIsHoveringUpload(false)}>
+      {/* Container Header */}
+      <div className="relative flex flex-col gap-2">
+        <h3 className="text-2xl font-bold text-[var(--text-primary)]">
+          Get Started
+        </h3>
+        {/* Underscore */}
+        <div className="h-1.5 w-[60%] rounded-md bg-[var(--container-secondary)]"></div>
+        {/* Vector Image */}
+        <img
+          className="transition-position absolute top-[-5.65rem] left-0 z-[-1] h-30 w-30 object-contain duration-500 hover:top-[-8rem]"
+          style={{
+            top: isHoveringUpload ? "-8rem" : "-5.65rem",
+          }}
+          src={"Slug Book.png"}
+          draggable={false}
+        />
+      </div>
+
+      <UploadArea />
+    </section>
+  );
+}
