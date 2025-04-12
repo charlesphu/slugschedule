b2390d35f84e88257ae5cd33c63fa0d687e7b9c0
charlesphu <charlesphu18@gmail.com>
2025-04-12 10:08:08 -0700

    fixed majorRequriements request

--- app/api/gemini/route.js
+++ app/api/gemini/route.js
@@ -2,7 +2,7 @@ import { sendTxtAndPrompt } from "./utils/geminiHelper";
 
 export async function POST(request) {
   try {
-    const { requestType, pdfText, urlText } = await request.json();
+    const { requestType, pdfText } = await request.json();
     let prompt = null;
 
     if (requestType == "pdf") {
@@ -16,12 +16,7 @@ export async function POST(request) {
         "This is a pdf that is the transcript of a student. Please parse the pdf for all of the classes that have been taken by a user, and return it in your response as an array. Your response is being returned as a json object so please be mindful of this and do not create any sort of disruptive writing or formatting. I just need your response as an array of taken class codes (e.g., CSE20, BIO120, etc.) as a comma-separated list. " +
         pdfText;
     } else if (request == "majorRequirements") {
-      if (!urlText) {
-        return new Response(
-          JSON.stringify({ success: false, error: "PDF text is required." }),
-          { status: 400, headers: { "Content-Type": "application/json" } }
-        );
-      }
+      let urlText = null;
       prompt =
         "Please parse this to find all the course names and codes: " + urlText;
     }
