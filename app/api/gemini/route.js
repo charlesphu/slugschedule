fbcc9020a57b5eb0bb086862143bb9535c68e90d
charlesphu <charlesphu18@gmail.com>
2025-04-12 10:06:02 -0700

    more robust gemini POST function

--- app/api/gemini/route.js
+++ app/api/gemini/route.js
@@ -2,19 +2,30 @@ import { sendTxtAndPrompt } from "./utils/geminiHelper";
 
 export async function POST(request) {
   try {
-    const { pdfText } = await request.json();
+    const { requestType, pdfText, urlText } = await request.json();
+    let prompt = null;
 
-    if (!pdfText) {
-      return new Response(
-        JSON.stringify({ success: false, error: "PDF text is required." }),
-        { status: 400, headers: { "Content-Type": "application/json" } }
-      );
+    if (requestType == "pdf") {
+      if (!pdfText) {
+        return new Response(
+          JSON.stringify({ success: false, error: "PDF text is required." }),
+          { status: 400, headers: { "Content-Type": "application/json" } }
+        );
+      }
+      prompt =
+        "This is a pdf that is the transcript of a student. Please parse the pdf for all of the classes that have been taken by a user, and return it in your response as an array. Your response is being returned as a json object so please be mindful of this and do not create any sort of disruptive writing or formatting. I just need your response as an array of taken class codes (e.g., CSE20, BIO120, etc.) as a comma-separated list. " +
+        pdfText;
+    } else if (request == "majorRequirements") {
+      if (!urlText) {
+        return new Response(
+          JSON.stringify({ success: false, error: "PDF text is required." }),
+          { status: 400, headers: { "Content-Type": "application/json" } }
+        );
+      }
+      prompt =
+        "Please parse this to find all the course names and codes: " + urlText;
     }
 
-    const prompt =
-      "This is a pdf that is the transcript of a student. Please parse the pdf for all of the classes that have been taken by a user, and return it in your response as an array. Your response is being returned as a json object so please be mindful of this and do not create any sort of disruptive writing or formatting. I just need your response as an array of taken class codes (e.g., CSE20, BIO120, etc.) as a comma-separated list. " +
-      pdfText;
-    console.log(prompt);
     const geminiResponse = await sendTxtAndPrompt(prompt);
 
     return new Response(JSON.stringify(geminiResponse), {
@@ -24,7 +35,11 @@ export async function POST(request) {
   } catch (error) {
     console.error("Error in route.js:", error);
     return new Response(
-      JSON.stringify({ success: false, error: "Failed to process the request.", details: error.message }),
+      JSON.stringify({
+        success: false,
+        error: "Failed to process the request.",
+        details: error.message,
+      }),
       { status: 500, headers: { "Content-Type": "application/json" } }
     );
   }
